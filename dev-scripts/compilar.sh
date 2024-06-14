4ea303bae6e19af4e71373854576820b9f7aef81
willow <brmagrini@gmail.com>
2024-06-14 13:31:03 -0300

    altera header no style.css e pasta para hacklabr

--- dev-scripts/compilar.sh
+++ dev-scripts/compilar.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
-cd $DIR/../themes/teste-base
+cd $DIR/../themes/hacklabr
 docker run -it -v `pwd`:/compilar node:20 bash -c "cd compilar && npm install && npm run production"
--- dev-scripts/zip.sh
+++ dev-scripts/zip.sh
@@ -2,4 +2,4 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CDIR=$( pwd )
 cd $DIR/../themes
-zip -r ../zips/teste-base.zip teste-base -x "teste-base/node_modules/*"
+zip -r ../zips/hacklabr.zip hacklabr -x "hacklabr/node_modules/*"
